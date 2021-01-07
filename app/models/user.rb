295d076f46283bc7a8e6b97051aa49fa323bfcbe
Rei-A-38 <reidch7@gmail.com>
2021-01-07 14:06:04 +0900

    userモデル テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,17 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
-  
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do 
-   validates :kanji_sei
-   validates :kanji_mei
-   validates :katakana_sei
-   validates :katakana_mei
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :kanji_sei
+    validates :kanji_mei
+  end
+
+  with_options presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい' } do
+    validates :katakana_sei
+    validates :katakana_mei
   end
 
   validates :birthday, presence: true
