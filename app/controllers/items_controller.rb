0768a32ceb7e183a49c8863e720948115f0fd37a
Rei-A-38 <reidch7@gmail.com>
2021-01-10 21:50:09 +0900

    商品情報編集機能　修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  # before_action :move_to_index, only: [:edit]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show,:edit,:update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -42,6 +42,10 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :title, :detail, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
                                  :delivery_time_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   #   def move_to_index
