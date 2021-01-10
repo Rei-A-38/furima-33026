9d453bbd63b7fe7435c3c21f42301e94c0f062a3
Rei-A-38 <reidch7@gmail.com>
2021-01-10 21:28:08 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  # before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -22,10 +23,36 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless @item.user_id == current_user.id
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to action: :show
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:image, :title, :detail, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
                                  :delivery_time_id, :price).merge(user_id: current_user.id)
   end
+
+  #   def move_to_index
+  #     item = Item.find(params[:id])
+  #     # if current_user == nil
+  #     #   redirect_to  user_session_path
+  #     elsif
+  #       redirect_to root_path
+
+  #     else
+  #     end
+
+  #  end
 end
