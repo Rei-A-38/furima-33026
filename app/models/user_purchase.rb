b10c8e6dad511ccd18277677aa6ba38cd8ca523d
Rei-A-38 <reidch7@gmail.com>
2021-01-15 20:09:26 +0900

    商品購入機能テストコード実装

--- app/models/user_purchase.rb
+++ app/models/user_purchase.rb
@@ -8,7 +8,7 @@ class UserPurchase
   with_options presence: true do
     validates :user_id
     validates :item_id
-    validates :prefecture_id,         numericality: { other_than: 0 }
+    validates :prefecture_id,         numericality: { other_than: 1 }
     validates :postal_code,           format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :city,                  format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "can't be blank" }
     validates :house_number         
