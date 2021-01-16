14591737ecf1ea8eda2ae7d3d1d301818ddee45b
Rei-A-38 <reidch7@gmail.com>
2021-01-16 16:21:27 +0900

    商品購入機能　修正５

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,7 +23,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to root_path unless @item.user_id == current_user.id
+    # redirect_to root_path unless @item.user_id == current_user.id
+    redirect_to root_path if current_user.id == @item.user_id && !@item.purchase.nil?
+    # return redirect_to root_path if current_user.id == @item.user_id || !@item.purchase.nil?
   end
 
   def update
